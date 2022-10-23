e2ccbf0866fc460fd00122efa2c59fb990b8b7ee
Deng YF <1240517822@qq.com>
2022-10-23 21:14:11 +0800

    更改博客名字和logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,13 @@ const pluginsConf = require('./config/plugins/index')
 module.exports = {
     //注意，此处需要填写你部署在nginx下的文件夹名称，如果是根目录，那么可以注释掉此行，注释掉后本地打开index.html无法访问
     //base: "/dist/",
-    title: "君哥聊编程",
+    title: "大飞个人博客",
     description: '点赞、转发、收藏',
-    port: '7777',
     head: [
         ['link', { rel: 'icon', href: '/img/favicon.ico' }],
-        ['meta', { name: 'keywords', content: '君哥聊编程,vuepress,自建博客,君哥' }],
-        ['meta', { name: 'description', content: '专属于自学者的在线学习平台,这里有编程领域最完善最「体系化的」Java学习视频、如果你是小白快加入我们一起学最全最「体系化的」java知识吧，官方交流QQ群：827553720' }],
+        ['meta', { name: 'keywords', content: '大飞聊编程,vuepress,自建博客,大飞' }],
         ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ["meta", { name: "author", content: "君哥" }],
+        ["meta", { name: "author", content: "大飞" }],
         ["link", { rel: "stylesheet", href: "/css/style.css" }],//显示nav小logo
         // ["script", { charset: "utf-8", src: "/js/custom.js" }],//加载左侧菜单栏图片
     ],
@@ -21,8 +19,8 @@ module.exports = {
         smoothScroll: true,
         authorAvatar: '/avatar.png',
         // 最后更新时间
-        lastUpdated: '2021-04-07', // string | boolean
-        author: '君哥',
+        lastUpdated: '上次更新', // string | boolean
+        author: '大飞',
         // 项目开始时间
         startYear: '2022',
         nav: navConf,
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,8 @@
 ---
 home: true
-heroText: '粉丝的个人博客'
+heroText: '大飞的个人博客'
 bgImage: '/img/5.jpg'
-bgImageStyle: {
-  height: '350px'
-}
+bgImageStyle: { height: '350px' }
 # faceImage: '/head.png'
 # heroImage: '/hero_white.png'
 # heroImageStyle: {
@@ -18,11 +16,11 @@ isShowTitleInHome: true
 actionText: About Me
 actionLink: /views/personalInfo/
 features:
-- title: Who am I？
-  details: A programmer? A human being? Or a matter combination?
-- title: Where am I？
-  details: In a city? On earth? Or in a small part of space?
-- title: What am I gona do？
-  details: Work? Struggle? Or go to a destination I don't know?
+  - title: Who am I？
+    details: A programmer? A human being? Or a matter combination?
+  - title: Where am I？
+    details: In a city? On earth? Or in a small part of space?
+  - title: What am I gona do？
+    details: Work? Struggle? Or go to a destination I don't know?
 # footer: 'reco_luan'
 ---
